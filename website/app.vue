8f8fa15c374a67aa62ec61d4d263d785d4d74eea
Joel McCandless <mail@joelmccandless.com>
2023-12-16 15:24:37 -0500

    add basic header and footer outline

--- website/app.vue
+++ website/app.vue
@@ -1,5 +1,30 @@
 <template>
+  <!-- common layout for each page -->
   <div>
-    <NuxtWelcome />
+    
+    <!-- define header -->
+    <header>
+      <!-- navigation links -->
+      <nav>
+	<ul>
+	  <li><NuxtLink to="/about">About Me</NuxtLink></li>
+	  <li><NuxtLink to="/mission">Mission</NuxtLink></li>
+	  <li><NuxtLink to="/projects">Projects</NuxtLink></li>
+	  <li><NuxtLink to="/collaboration">Social</NuxtLink></li>
+	  <li><NuxtLink to="/timeline">Timeline</NuxtLink></li>
+	</ul>
+      </nav>
+    </header>
+
+    <body style="min-height: 85vh">
+      <!-- page content -->
+      <NuxtPage />
+    </body>
+
+    <!-- define footer -->
+    <footer>
+      <p>Contact Me!</p>
+    </footer>
+    
   </div>
 </template>
