95383b939896dc09390530097684305ec0e0c56d
Joel McCandless <mail@joelmccandless.com>
2023-12-17 14:35:03 -0500

    add single tag to footer

--- website/app.vue
+++ website/app.vue
@@ -3,27 +3,35 @@
   <div>
     
     <!-- define header -->
-    <header>
+    <header class="p-4 shadow-lg">
       <!-- navigation links -->
-      <nav>
-	<ul>
+      <nav class="container flex">
+	<ul class="flex space-x-8">
 	  <li><NuxtLink to="/about">About Me</NuxtLink></li>
 	  <li><NuxtLink to="/mission">Mission</NuxtLink></li>
 	  <li><NuxtLink to="/projects">Projects</NuxtLink></li>
-	  <li><NuxtLink to="/collaboration">Social</NuxtLink></li>
+	  <li><NuxtLink to="/social">Social</NuxtLink></li>
 	  <li><NuxtLink to="/timeline">Timeline</NuxtLink></li>
 	</ul>
       </nav>
     </header>
 
-    <body style="min-height: 85vh">
+    <body style="min-height: 80vh">
       <!-- page content -->
       <NuxtPage />
     </body>
 
     <!-- define footer -->
-    <footer>
-      <p>Contact Me!</p>
+    <footer class="w-full flex flex-row items-center border-t-4 p-4">
+      <div class="pr-4">Contact Me!</div>
+      <ul class="flex space-x-8">
+	<li>
+	  <NuxtLink to="https://github.com/jmccand" target="_blank" class="border-2 rounded-md p-2 flex flex-row items-center">
+	    <img src="@/assets/images/github-icon.png" style="max-height: 4vh" class="pr-2">
+	    Jmccand
+	  </NuxtLink>
+	</li>
+      </ul>
     </footer>
     
   </div>
