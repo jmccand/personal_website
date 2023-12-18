68ffc4acfa8acc236022280461bce65ab9b29202
Joel McCandless <mail@joelmccandless.com>
2023-12-17 21:58:26 -0500

    make about page responsive

--- website/pages/about.vue
+++ website/pages/about.vue
@@ -1,11 +1,11 @@
 <template>
   <main class="p-4 px-6">
-    <div class="flex flex-row items-center">
+    <div class="flex flex-col-reverse items-center md:flex-row">
       <p class="px-4 text-xl">
 	<h1 class="text-5xl font-bold pb-8">About Me</h1>
 	Currently a student at Rensselaer Polytechnic Institute, I'm pursuing a Dual major in Computer Science and Computer Engineering. Beyond classes, I also work with professors and collegues on projects and play tennis for the RPI Men's Tennis Team.
       </p>
-      <img src="@/assets/images/cropped_joel.jpg" style="max-height:60vh">
+      <img src="@/assets/images/cropped_joel.jpg" style="max-height:60vh" class="pb-8 md:pb-0">
     </div>
   </main>
 </template>
