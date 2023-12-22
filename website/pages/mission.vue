f5292d6c2f378fd09b538fb88392581d29fa4b5b
Joel McCandless <mail@joelmccandless.com>
2023-12-21 19:16:50 -0500

    add mission statement

--- website/pages/mission.vue
+++ website/pages/mission.vue
@@ -1,4 +1,8 @@
 <template>
-  <main>
+  <main class="text-center text-4xl p-20">
+    <h1 class="leading-relaxed">
+      I will maintain a boundless curiousity for learning by embracing the excitement found in exploration. I will acquire both a breadth and depth of skills and unite fields in novel ways. I will maintain a standard for myself which builds self-esteem and pride in my accomplishments. I will give off gratitude for life and form friendships with those around me in an effort to grow together.
+    </h1>
+    <p class="pt-16">-- Joel McCandless --</p>
   </main>
 </template>
