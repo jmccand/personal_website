83c8337b2df01797bf3f9c74b8189653d60cd53d
Joel McCandless <mail@joelmccandless.com>
2024-01-03 22:34:29 -0500

    switch to flex divs instead of table

--- website/pages/projects.vue
+++ website/pages/projects.vue
@@ -3,21 +3,19 @@
     <div class="border-4">
       <h1 class="text-5xl font-bold py-12">My Projects</h1>
     </div>
-    <table class="text-left border-separate border-spacing-y-4">
-      <tbody>
-	<tr v-for="project in projects">
-	  <td class="border-2 border-r-0 p-2">
-	    <span class="font-bold pr-4">{{ project.name }}</span><span class="pr-1">{{ project.start }}</span>-<span class="pl-1">{{ project.end }}</span><br />
-	    <span>{{ project.goal }}</span><br />
-	    <p v-html="project.details"></p>
-	    <p><span class="font-bold pr-2">More</span><span v-for="more_item in project.more"><MoreLink :to="more_item[1]">{{ more_item[0] }}</MoreLink></span></p>
-	  </td>
-	  <td class="border-2 border-l-0 p-2">
-	    <img :src="'/_nuxt/assets/images/' + project.thumbnail" :alt="project.name" style="max-height: 100vh">
-	  </td>
-	</tr>
-      </tbody>
-    </table>
+    <div class="text-left w-full pt-4">
+      <div v-for="project in projects" class="mb-4 flex flex-col sm:flex-row border-2 items-center">
+	<div class="p-2 pr-0">
+	  <span class="font-bold pr-4">{{ project.name }}</span><span class="pr-1">{{ project.start }}</span>-<span class="pl-1">{{ project.end }}</span><br />
+	  <span>{{ project.goal }}</span><br />
+	  <p v-html="project.details" class="line-clamp-5"></p>
+	  <p><span class="font-bold pr-2">More</span><span v-for="more_item in project.more"><MoreLink :to="more_item[1]">{{ more_item[0] }}</MoreLink></span></p>
+	</div>
+	<div class="p-2 h-full">
+	  <img :src="'/_nuxt/assets/images/' + project.thumbnail" :alt="project.name" class="max-h-[12rem] max-w-[12rem] min-w-[6rem] object-cover">
+	</div>
+      </div>
+    </div>
   </main>
 </template>
 
