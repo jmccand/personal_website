872855eb5bc77d4409b66cc6cbc33ad089c8c006
Joel McCandless <mail@joelmccandless.com>
2024-03-04 22:39:57 -0500

    add image to popup

--- website/pages/projects.vue
+++ website/pages/projects.vue
@@ -18,10 +18,15 @@
 	</div>
       </template>
     </div>
-    <div v-if="displayed_project != null" id="popup" class="text-left rounded-md border-4 fixed bg-white right-4 left-4 height-0 top-0 p-4 overflow-hidden transition-all duration-1000" :style="{ top: targetTop+'px', height: targetHeight+'px' }">
+    <div v-if="displayed_project != null" id="popup" class="text-left rounded-md border-4 fixed bg-white right-4 left-4 height-0 top-0 p-4 overflow-y-scroll transition-all duration-1000" :style="{ top: targetTop+'px', height: targetHeight+'px' }">
       <span class="font-bold pr-4">{{ displayed_project.name }}</span><span class="pr-1">{{ displayed_project.start }}</span>-<span class="pl-1">{{ displayed_project.end }}</span><br />
       <span>{{ displayed_project.goal }}</span><br />
       <p v-html="displayed_project.details"></p>
+      <br />
+      <div class="flex flex-row w-100 items-center justify-center">
+	<img :src="'/images/' + displayed_image" class="max-h-[16rem] max-w-[30rem] min-h-[10rem] object-cover">
+      </div>
+      <br />
       <p><span class="font-bold pr-2">More</span><span v-for="more_item in displayed_project.more"><MoreLink :to="more_item[1]">{{ more_item[0] }}</MoreLink></span></p>
     </div>
   </main>
@@ -34,10 +39,12 @@
  var targetTop = ref(80);
  var targetHeight = ref(60);
  var displayed_project = ref(null);
+ var displayed_image = ref(null);
 
  async function update_displayed_project(elem_id) {
    var displayed_elem = document.getElementById(elem_id);
    displayed_project.value = findProjectByName(elem_id);
+   displayed_image.value = displayed_project.value.images[0];
    var rect = displayed_elem.getBoundingClientRect();
    // show initial box
    targetTop.value = rect.top - 10;
@@ -48,7 +55,7 @@
    setTimeout(() => {
      // expand popup
      targetTop.value = 80;
-     targetHeight.value = innerHeight - targetTop.value - 80;
+     targetHeight.value = innerHeight - targetTop.value - 40;
    }, 5);
  }
 
