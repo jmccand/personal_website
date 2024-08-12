e5dc8fef09c01929b86ea592f868ba6e05748e1d
Joel McCandless <mail@joelmccandless.com>
2024-08-12 18:30:08 -0400

    fix collegue typo

--- website/pages/about.vue
+++ website/pages/about.vue
@@ -3,7 +3,7 @@
     <div class="flex flex-col-reverse items-center md:flex-row">
       <p class="px-4 text-xl">
 	<h1 class="text-5xl font-bold pb-8">About Me</h1>
-	Currently a student at Rensselaer Polytechnic Institute, I'm pursuing a Dual major in Computer Science and Computer Engineering. Beyond classes, I also work with professors and collegues on projects and play tennis for the RPI Men's Tennis Team.
+	Currently a student at Rensselaer Polytechnic Institute, I'm pursuing a Dual major in Computer Science and Computer Engineering. Beyond classes, I also work with professors and colleagues on projects and play tennis for the RPI Men's Tennis Team.
       </p>
       <img src="/images/joel_serious.jpg" alt="joel.jpg" style="max-height:60vh" class="pb-8 md:pb-0">
     </div>
