88eb5aec181a81e75eddd586169a9695b941861d
Joel McCandless <mail@joelmccandless.com>
2024-08-18 15:39:39 -0400

    make mission statement font size responsive

--- website/pages/mission.vue
+++ website/pages/mission.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="text-center text-4xl p-20">
+  <main class="text-center text-lg md:text-4xl sm:text-2xl p-20">
     <h1 class="leading-relaxed">
       I will maintain a boundless curiosity for learning by embracing the excitement found in exploration. I will acquire both a breadth and depth of skills and unite fields in novel ways. I will maintain a standard for myself which builds self-esteem and pride in my accomplishments. I will give off gratitude for life and form friendships with those around me in an effort to grow together.
     </h1>
