34a99dac2c1af012aa5ca86ed8e9900a52e0e4fe
Joel McCandless <mail@joelmccandless.com>
2024-08-18 16:01:40 -0400

    fix footer tag centering

--- website/app.vue
+++ website/app.vue
@@ -38,25 +38,25 @@
     </body>
 
     <!-- define footer -->
-    <footer class="w-full flex flex-col md:flex-row items-center border-t-4 p-4 space-x-4 space-y-2 text-xs lg:text-base">
+    <footer class="w-full flex flex-col md:flex-row items-center border-t-4 p-4 sm:space-x-4 space-y-2 sm:space-y-0 text-xs lg:text-base">
       <TagLink :to="'https://www.linkedin.com/in/joel-mccandless-a09275273/'">
-        <img src="/images/linkedin-icon.png" style="max-height: 3vh" class="pr-2" />
+        <img src="/images/linkedin-icon.png" style="max-height: 3vh" class="sm:pr-2" />
         LinkedIn
       </TagLink>
       <TagLink :to="'https://github.com/jmccand'">
-        <img src="/images/github-icon.png" style="max-height: 3vh" class="pr-2" />
+        <img src="/images/github-icon.png" style="max-height: 3vh" class="sm:pr-2" />
         GitHub/Jmccand
       </TagLink>
       <TagLink :to="'mailto:mail@joelmccandless.com'">
-        <img src="/images/mail-icon.png" style="max-height: 4vh" class="pr-2" />
+        <img src="/images/mail-icon.png" style="max-height: 4vh" class="sm:pr-2" />
         mail@joelmccandless.com
       </TagLink>
       <TagLink :to="'tel:+7812672701'">
-        <img src="/images/phone-icon.png" style="max-height: 3vh" class="pr-2" />
+        <img src="/images/phone-icon.png" style="max-height: 3vh" class="sm:pr-2" />
         (781) 267-2701
       </TagLink>
       <TagLink :to="'https://goo.gl/maps/QgktqPpbrLqry4Mz9'">
-        <img src="/images/location-icon.png" style="max-height: 3vh" class="pr-2" />
+        <img src="/images/location-icon.png" style="max-height: 3vh" class="sm:pr-2" />
         Lexington, MA
       </TagLink>
     </footer>
