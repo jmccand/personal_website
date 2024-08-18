561bb1dafed3e6a8f3e307a63033b246eb14033a
Joel McCandless <mail@joelmccandless.com>
2024-08-17 23:04:11 -0400

    expand project in place onclick

--- website/pages/projects.vue
+++ website/pages/projects.vue
@@ -4,8 +4,8 @@
       <h1 class="text-5xl font-bold py-12">My Projects</h1>
     </div>
     <div class="text-left w-full pt-4">
-      <template v-for="project in projects">
-      <div :id="project.name" class="cursor-pointer mb-4 flex flex-col sm:flex-row border-2 items-center" @click="(event) => update_displayed_project(project.name, event)">
+      <template v-for="(project, index) in projects">
+      <div v-if="!expanded[index][0]" :id="project.name" class="cursor-pointer mb-4 flex flex-col sm:flex-row border-2 items-center" @click="() => expanded[index] = [true, 0]">
         <div class="p-2 pr-0">
           <span class="font-bold pr-4">{{ project.name }}</span><span class="pr-1">{{ project.start }}</span>-<span class="pl-1">{{ project.end }}</span><br />
           <span>{{ project.goal }}</span><br />
@@ -16,89 +16,34 @@
           <img :src="'/images/' + project.images[0]" :alt="project.name" class="max-h-[12rem] max-w-[12rem] min-w-[6rem] object-cover" />
         </div>
       </div>
-      </template>
-    </div>
-    <div v-if="displayed_project != null" id="popup" class="text-left rounded-md border-4 fixed bg-white right-4 left-4 height-0 top-0 p-4 overflow-y-scroll transition-all duration-1000" :style="{ top: targetTop+'px' }">
-      <span class="font-bold pr-4">{{ displayed_project.name }}</span><span class="pr-1">{{ displayed_project.start }}</span>-<span class="pl-1">{{ displayed_project.end }}</span><span class="absolute right-4 top-4 font-sans cursor-pointer p-2" @click="close_popup()">&#9587</span> <br />
-      <span>{{ displayed_project.goal }}</span><br />
-      <p v-html="displayed_project.details"></p>
-      <br />
-      <div class="flex flex-row gap-5 items-stretch w-100 items-center justify-center">
-        <div v-if="displayed_image_index > 0" class="flex w-10 text-center items-center justify-items-center bg-slate-200 animate-pulse cursor-pointer" @click="(event) => {event.stopPropagation(); displayed_image_index--; }">
-          <p class="w-full rotate-180">&#10140</p>
+      <div v-else class="cursor-pointer mb-4 border-2 p-2" @click="() => expanded[index] = [false, 0]">
+        <span class="font-bold pr-4">{{ project.name }}</span><span class="pr-1">{{ project.start }}</span>-<span class="pl-1">{{ project.end }}</span><span class="absolute right-4 top-4 font-sans cursor-pointer p-2" @click="close_popup()">&#9587</span> <br />
+        <span>{{ project.goal }}</span><br />
+        <p v-html="project.details"></p>
+        <br>
+        <div class="flex flex-row gap-5 items-stretch w-100 items-center justify-center">
+          <div v-if="expanded[index][1] > 0" class="flex w-10 text-center items-center justify-items-center bg-slate-200 animate-pulse cursor-pointer" @click="(event) => {event.stopPropagation(); expanded[index][1]--; }">
+            <p class="w-full rotate-180">&#10140</p>
+          </div>
+          <div v-else class="w-10"></div>
+          <img :src="'/images/' + project.images[expanded[index][1]]" class="max-h-[16rem] max-w-[30rem] min-h-[10rem] object-cover" />
+          <div v-if="expanded[index][1] < project.images.length - 1" class="flex w-10 text-center items-center justify-items-center bg-slate-200 animate-pulse cursor-pointer" @click="(event) => {event.stopPropagation(); expanded[index][1]++; }">
+            <p class="w-full">&#10140</p>
+          </div>
+          <div v-else class="w-10">
+          </div>
         </div>
-        <div v-else class="w-10">
-        </div>
-        <img :src="'/images/' + displayed_project.images[displayed_image_index]" class="max-h-[16rem] max-w-[30rem] min-h-[10rem] object-cover" />
-        <div v-if="displayed_image_index < displayed_project.images.length - 1" class="flex w-10 text-center items-center justify-items-center bg-slate-200 animate-pulse cursor-pointer" @click="(event) => {event.stopPropagation(); displayed_image_index++; }">
-          <p class="w-full">&#10140</p>
-        </div>
-        <div v-else class="w-10">
-        </div>
+        <br>
+        <p><span class="font-bold pr-2">More</span><span v-for="more_item in project.more"><MoreLink :to="more_item[1]">{{ more_item[0] }}</MoreLink></span></p>
       </div>
-      <br />
-      <p><span class="font-bold pr-2">More</span><span v-for="more_item in displayed_project.more"><MoreLink :to="more_item[1]">{{ more_item[0] }}</MoreLink></span></p>
+      </template>
     </div>
   </main>
 </template>
 
 <script setup>
  import projects from "@/assets/data/projects.json"
- import { ref, onUpdated, nextTick } from 'vue'
-
- var targetTop = ref(80);
- var targetHeight = ref(60);
- var displayed_project = ref(null);
- var displayed_image_index = ref(null);
  
- async function update_displayed_project(elem_id, event) {
-   const popup = document.getElementById('popup');
-   if (!popup) {
-    var displayed_elem = document.getElementById(elem_id);
-    displayed_project.value = findProjectByName(elem_id);
-    displayed_image_index.value = 0;
-    var rect = displayed_elem.getBoundingClientRect();
-    // show initial box
-    targetTop.value = rect.top - 10;
-    targetHeight.value = displayed_elem.offsetHeight;
-    
-    // prevent scrolling
-    document.body.style.overflow = "hidden";
-
-    // wait for box to be displayed on DOM
-    await nextTick();
-    setTimeout(() => {
-      // expand popup
-      targetTop.value = 80;
-      // targetHeight.value = innerHeight - targetTop.value - 40;
-    }, 5);
-
-    event.stopPropagation();
-
-    window.addEventListener('click', function(event) {
-      const popup = document.getElementById('popup');
-      console.log(event.target);
-      console.log(popup);
-      if (popup && !popup.contains(event.target)) {
-        close_popup();
-      }
-    });
-   }
- }
-
- async function close_popup() {
-   displayed_project.value = null;
-   displayed_image_index.value = 0;
-   document.body.style.overflow = "auto";
- }
-
- function findProjectByName(elem_id) {
-   for (const [index, element] of projects.entries()) {
-     if (element.name == elem_id) {
-       return element;
-     }
-   }
-   return null;
- }
+ var expanded = ref(projects.map(() => [false, 0]));
  
 </script>
